36e39018254ce6a7ffd4d03b689766fa6207f0ef
tyd <da36720889@gmail.com>
2025-10-08 23:30:02 +0800

    Add files via upload

---
+++ jetson_gui/main.cpp
@@ -0,0 +1,16 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
+
+
+// void MainWindow::pb_start_clicked(){
+//     mainwin->graphicview->
+// }
